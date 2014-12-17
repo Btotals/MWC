b7d9edccc673fc2b168ec32a329f51cf42baa74b
Sapphire <qianyedaodaozhai@163.com>
2014-12-17 15:09:01 +0800

    3:08

--- meteor/leaderboard1/leaderboard.js
+++ meteor/leaderboard1/leaderboard.js
@@ -3,23 +3,33 @@ players = new Mongo.Collection('players');
 if (Meteor.isClient) {
 	Template.leaderboard.helpers({
 		players: function() {
-			return players.find();
+			return players.find({}, {
+				sort: {
+					score: -1,
+					name: 1
+				}
+			});
+			console.log(players.find());
 		}
 	});
+
+	Template.leaderboard.events({
+		'submit .new-player': function(event, template) {
+			var input = event.target.find('input');
+		}
+	})
 }
 
 if (Meteor.isServer) {
 	Meteor.startup(function() {
 		if (players.find().count() == 0) {
 			var name = "MD";
-			var score = 5;
 			for (var i = 0; i < 5; i++) {
 				players.insert({
 					name: name.toString(),
-					score: score.toString()
+					score: Math.floor(Math.random() * 10) * 5
 				})
 				name += "D";
-				score += 5;
 			}
 		}
 	});
