bb90697ebfc6c92c4b28847493aeaa54d2901b39
Sapphire <qianyedaodaozhai@163.com>
2014-12-18 14:39:38 +0800

    12-18

--- meteor/leaderboard1/leaderboard.js
+++ meteor/leaderboard1/leaderboard.js
@@ -9,15 +9,46 @@ if (Meteor.isClient) {
 					name: 1
 				}
 			});
-			console.log(players.find());
+		},
+		selectedName: function() {
+			var player = players.findOne(Session.get('selectedPlayer'));
+			return player != null ? player.name : void 0;
 		}
 	});
 
 	Template.leaderboard.events({
 		'submit .new-player': function(event, template) {
-			var input = event.target.find('input');
+			var input = $(event.target).find('input');
+			var playerName = input.val();
+			players.insert({
+				name: playerName,
+				score: Math.floor(Math.random() * 10) *5
+			});
+		},
+
+		'click .inc': function() {
+			return players.update(Session.get('selectedPlayer'), {
+				$inc: {
+					score: 5
+				}
+			});
+		}
+	});
+
+	Template.player.helpers({
+		selected: function() {
+			if (Session.equals('selectedPlayer', this._id)) {
+				return 'selected';
+			}
+		}
+	});
+
+	Template.player.events({
+		'click' : function() {
+			Session.set('selectedPlayer', this._id);
 		}
 	})
+
 }
 
 if (Meteor.isServer) {
